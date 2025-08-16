8f36046b4bdcf0c5ed6ac499ab5a9e52b1b99de5
Francisco-Krossler <krosslerfrancisco@gmail.com>
2025-08-16 16:33:33 -0300

    v0.1

--- components/character/player.gd
+++ components/character/player.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const Lower = +400
-const JUMP_VELOCITY = -300.0 
-const BIG_JUMP_VELOCITY = -360.0
+@onready var data: PlayerData = $PlayerData
+@onready var anim = $animations
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var was_on_floor = true
 var landing_timer = 0.0
@@ -11,57 +10,51 @@ var attacking = false
 var cursor_texture = load("res://assets/cursor/cursor_teste_2.png")
 
 func _ready():
-	$animations.connect("animation_finished", Callable(self, "_on_animation_finished"))
-
+	anim.connect("animation_finished", Callable(self, "_on_animation_finished"))
 	Input.set_custom_mouse_cursor(cursor_texture)
+
 func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 
-	# Gravedad
+	# Gravidade
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Salto
+	# Salto normal
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and not attacking:
-		velocity.y = JUMP_VELOCITY
-		
-	#super pulo apertando seta pra cima
+		velocity.y = data.jump_velocity
+
+	# Super pulo
 	if Input.is_action_just_pressed("ui_up") and is_on_floor() and not attacking:
-		velocity.y = BIG_JUMP_VELOCITY
-		
-	# abaixar
+		velocity.y = data.big_jump_velocity
+
+	# Abaixar
 	if Input.is_action_just_pressed("ui_down") and not attacking:
-		velocity.y = Lower
-		
-	# Ataque (solo una vez)
-	# Ataque (solo una vez)
+		velocity.y = data.lower_force
+
+	# Ataque
 	if Input.is_action_just_pressed("click_esquerdo") and not attacking:
 		attacking = true
 		velocity.x = 0
-		
-		# vira o personagem para a direção do mouse
-		if get_global_mouse_position().x < global_position.x:
-			$animations.flip_h = true
-		else:
-			$animations.flip_h = false
 
-		$animations.play("attack")
+		anim.flip_h = get_global_mouse_position().x < global_position.x
+		anim.play("attack")
 		return
 
-	# Movimiento horizontal y rotación
+	# Movimento horizontal
 	if not attacking:
 		if direction:
-			velocity.x = direction * SPEED
-			$animations.flip_h = direction < 0
+			velocity.x = direction * data.speed
+			anim.flip_h = direction < 0
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = move_toward(velocity.x, 0, data.speed)
 
 	move_and_slide()
 
-	# Animaciones solo si no está atacando
+	# Animações
 	if not attacking:
 		if is_on_floor() and not was_on_floor:
-			$animations.play("jump_down")
+			anim.play("jump_down")
 			landing_timer = 0.2
 		elif landing_timer > 0:
 			landing_timer -= delta
@@ -73,16 +66,15 @@ func _physics_process(delta):
 func decide_animation(direction):
 	if is_on_floor():
 		if direction != 0:
-			$animations.play("run")
+			anim.play("run")
 		else:
-			$animations.play("idle")
+			anim.play("idle")
 	else:
 		if velocity.y < 0:
-			$animations.play("jump_up")
+			anim.play("jump_up")
 		else:
-			$animations.play("falling")
+			anim.play("falling")
 
 func _on_animation_finished():
-	# Liberar ataque solo si la animación terminada es "attack"
-	if $animations.animation == "attack":
+	if anim.animation == "attack":
 		attacking = false
---
+++ components/character/player_data.gd
@@ -0,0 +1,8 @@
+# PlayerData.gd
+extends Node
+class_name PlayerData
+
+@export var speed: float = 350.0
+@export var jump_velocity: float = -400.0
+@export var big_jump_velocity: float = -560.0
+@export var lower_force: float = 500.0
